43b23a5b647cc37a343803f05502a0a8a863857b
yagogarea <yagogareacidre@gmail.com>
2025-03-26 20:30:37 +0100

    test: add total user time to test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+total_time=0
+
 for i in $(seq -f "%02g" 1 11); do
   echo "---------------------"
   echo "Running decode $i..."
-  user_time=$((time make decode $i) 2>&1 | grep "user" | cut -f2)
+  user_time=$((time make decode $i) 2>&1 | grep "user" | awk -F'm' '{print $1*60*1000 + $2*1000}' )
+  total_time=$((total_time + user_time))
   
   if cmp -s "sol$i.txt" "examples/sol$i.txt"; then
     echo "Test dom$i passed"
@@ -13,6 +16,10 @@ for i in $(seq -f "%02g" 1 11); do
     echo "Test dom$i failed"
     break
   fi
-   echo "User time: $user_time"
+   echo "User time: $user_time ms"
   echo "---------------------"
 done
+
+echo "---------------------"
+echo "Total TUser: $total_time ms"
+echo "---------------------"
